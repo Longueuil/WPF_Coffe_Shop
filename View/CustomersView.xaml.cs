0dbdf66cfee644ae53cee9469b9d927e52b931a3
Vlad <vladimirtocarica@gmail.com>
2023-10-16 19:25:22 -0400

    view added

---
+++ View/CustomersView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CoffeShop.View
+{
+   
+    public partial class CustomersView : UserControl
+    {
+        public CustomersView()
+        {
+            InitializeComponent();
+        }
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            var column = Grid.GetColumn(gridCustomerList);
+
+            var newColumn = column == 0 ? 2 : 0;
+
+            Grid.SetColumn(gridCustomerList, newColumn);
+        }
+    }
+}
